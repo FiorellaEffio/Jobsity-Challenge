0283db304bb58c355248ed8198902962f33684ee
Aweba13 <fiorella.effio@unmsm.edu.pe>
2018-10-27 15:52:30 -0500

    adding a name and 2 params

--- src/components/VideoEditor.vue
+++ src/components/VideoEditor.vue
@@ -2,15 +2,15 @@
   <v-container fluid>
     <h1>Hi! You can create clips from this video</h1>
     <video width="400" controls :src="URLwithParams"></video>
-    <p>Please fill this information below</p>     
+    <p>Please fill this information below</p>    
+    <input type="text" v-model="currentClipName" placeholder="Give a name to your clip">
     <input type="text" v-model="initTime" placeholder="Init Time">
     <input type="text" v-model="finalTime" placeholder="Final Time">
     <button @click="createClip()">Create a clip</button>
     <br>
-    {{clips}}
-    <div v-for="clip in clips" :key="clip">
-      {{clip}}
-      <video autoplay width="400" :src="clip"></video>
+    <div v-for="clip in clips" :key="clip.clipName">
+      {{clip.clipName}}
+      <video autoplay width="400" :src="clip.urlTime"></video>
     </div>
   </v-container>
 </template>
@@ -23,13 +23,17 @@ export default {
         defaultURL: 'https://firebasestorage.googleapis.com/v0/b/jobsity-challenge.appspot.com/o/test%2Fsintel_trailer-480p.mp4?alt=media&token=fd2e61e1-f77f-4fa6-95f3-2f8d97532eaf',
         initTime: '0',
         finalTime: '0',
-        clips:['https://firebasestorage.googleapis.com/v0/b/jobsity-challenge.appspot.com/o/test%2Fsintel_trailer-480p.mp4?alt=media&token=fd2e61e1-f77f-4fa6-95f3-2f8d97532eaf#t=0,52'],
+        currentClipName: '',
+        clips: [{urlTime: 'https://firebasestorage.googleapis.com/v0/b/jobsity-challenge.appspot.com/o/test%2Fsintel_trailer-480p.mp4?alt=media&token=fd2e61e1-f77f-4fa6-95f3-2f8d97532eaf#t=0,52', clipName: this.currentClipName}]
       }
     },
   methods:{
     createClip: function() {
-      console.log(this.URLwithParams)
-      this.clips.push(this.URLwithParams);
+      let newClip = {
+        urlTime: this.URLwithParams,
+        clipName: this.currentClipName
+      }
+      this.clips.push(newClip);
     }
   },
   computed: {
